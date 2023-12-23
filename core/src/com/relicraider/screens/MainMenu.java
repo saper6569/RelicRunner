bf9eecc81546689bb6de5c0298866025a5bba605
Admin <saper6569@gmail.com>
2023-12-22 22:00:20 -0500

    documentation for Main Menu

--- core/src/com/relicraider/screens/MainMenu.java
+++ core/src/com/relicraider/screens/MainMenu.java
@@ -61,15 +61,20 @@ public class MainMenu implements Screen {
     public AssetManagers manager = new AssetManagers();
 
     @Override
+    /**
+     * This method contains any code that is run before the Main Menu is shown to the user
+     */
     public void show() {
         //stage for game resources
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
         //resource instances
+        //all reasources are loaded by the asset manager all at the beginning of the new screen
         manager.mainMenuAssets();
         manager.assetManager.finishLoading();
 
+        //load resources from disk
         settingsTexture = manager.assetManager.get(manager.settingsButton, Texture.class);
         creditsTexture = manager.assetManager.get(manager.creditsButton, Texture.class);
         playTexture = manager.assetManager.get(manager.playButton, Texture.class);
@@ -77,30 +82,37 @@ public class MainMenu implements Screen {
         backdropTexture = manager.assetManager.get(manager.menuBackdrop, Texture.class);
         menuSong = manager.assetManager.get(manager.mainMenuSong1, Music.class);
 
+        //camera objects for creating view of game for user
         camera = new OrthographicCamera();
         viewport = new FitViewport(SetupVariables.WIDTH, SetupVariables.HEIGHT, camera);
 
-        int origin_x = (Gdx.graphics.getWidth() - playTexture.getWidth()) / 2;
-        int origin_y = (Gdx.graphics.getHeight() - playTexture.getHeight()) / 2;
-
+        //create stage object for placing graphics on
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
+        //temp variables for placing graphics at disorder locations
+        int origin_x = (Gdx.graphics.getWidth() - playTexture.getWidth()) / 2;
+        int origin_y = (Gdx.graphics.getHeight() - playTexture.getHeight()) / 2;
+        //code for creating the play image button and placing it at the desired location
         playTextureRegion = new TextureRegion(playTexture);
         playDrawable = new TextureRegionDrawable(playTextureRegion);
         play = new ImageButton(playDrawable);
         play.setPosition(origin_x, origin_y);
+
+        //click listener to find when the user wants to switch to the first game screen
         play.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
 
                 ((Game)Gdx.app.getApplicationListener()).setScreen(new GameScreen1(game));
+                //stop the music if it is playing
                 if (menuSong.isPlaying()) {
                     menuSong.stop();
                 }
             }
         });
 
+        //code for creating the credits image button and placing it at the desired location
         origin_x = (Gdx.graphics.getWidth() - creditsTexture.getWidth()) / 2;
         origin_y = ((Gdx.graphics.getHeight() - creditsTexture.getHeight()) / 2) + 50;
         creditsTextureRegion = new TextureRegion(creditsTexture);
@@ -108,17 +120,20 @@ public class MainMenu implements Screen {
         credits = new ImageButton(creditsDrawable);
         credits.setPosition(origin_x, origin_y);
 
+        //click listener to find when the user wants to switch to the credits screen
         credits.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
 
                 ((Game)Gdx.app.getApplicationListener()).setScreen(new Credits(game));
+                //stop the music if it is playing
                 if (menuSong.isPlaying()) {
                     menuSong.stop();
                 }
             }
         });
 
+        //code for creating the settings image button and placing it at the desired location
         origin_x = (Gdx.graphics.getWidth() - settingsTexture.getWidth()) / 2;
         origin_y = ((Gdx.graphics.getHeight() - settingsTexture.getHeight()) / 2) - 50;
         settingsTextureRegion = new TextureRegion(settingsTexture);
@@ -126,17 +141,20 @@ public class MainMenu implements Screen {
         settings = new ImageButton(settingsDrawable);
         settings.setPosition(origin_x, origin_y);
 
+        //click listener to find when the user wants to switch to the settings screen
         settings.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
 
                 ((Game)Gdx.app.getApplicationListener()).setScreen(new Settings(game));
+                //stop the music if it is playing
                 if (menuSong.isPlaying()) {
                     menuSong.stop();
                 }
             }
         });
 
+        //code for creating the quit image button and placing it at the desired location
         origin_x = (Gdx.graphics.getWidth() - quitTexture.getWidth()) / 2;
         origin_y = ((Gdx.graphics.getHeight() - quitTexture.getHeight()) / 2) - 100;
         quitTextureRegion = new TextureRegion(quitTexture);
@@ -144,6 +162,7 @@ public class MainMenu implements Screen {
         quit = new ImageButton(quitDrawable);
         quit.setPosition(origin_x, origin_y);
 
+        //click listener to find when the user wants to exit
         quit.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -153,6 +172,7 @@ public class MainMenu implements Screen {
 
         backdrop = new Image(backdropTexture);
 
+        //add all graphics and music to the stage
         stage.addActor(backdrop);
         stage.addActor(play);
         stage.addActor(settings);
@@ -165,16 +185,20 @@ public class MainMenu implements Screen {
     }
 
     @Override
+    /**
+     * Render method for the Main Menu - resets and redraws the screen when called
+     */
     public void render(float delta) {
         //screen render functions
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //render the stage
         stage.act();
         stage.draw();
         countSec += delta;
 
-        //music
+        //loop through the menu song with a 300-second break between each repetition
         if (!menuSong.isPlaying() && countSec > 300) {
             menuSong = manager.assetManager.get(manager.mainMenuSong1, Music.class);
             menuSong.play();
@@ -205,6 +229,7 @@ public class MainMenu implements Screen {
 
     @Override
     public void dispose() {
+        //manual garbage disposal
         stage.dispose();
         playTexture.dispose();
         creditsTexture.dispose();
