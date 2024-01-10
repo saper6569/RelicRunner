b7fd0f6283f8d9653bc68206366a5fbeaf8fd3f6
rywes8339 <rywes8339@ugcloud.ca>
2024-01-10 10:29:36 -0500

    Changed Game Icon

--- desktop/src/com/relicraider/DesktopLauncher.java
+++ desktop/src/com/relicraider/DesktopLauncher.java
@@ -12,7 +12,7 @@ public class DesktopLauncher {
 		config.setForegroundFPS(60);
 		config.setTitle("Relic Raider");
 		config.setWindowPosition(650,250);
-		config.setWindowIcon(Files.FileType.Internal,"Other/fullHeart.png");
+		config.setWindowIcon(Files.FileType.Internal,"Sprites/relics.png");
 		new Lwjgl3Application(new RelicRaider(), config);
 	}
 }
