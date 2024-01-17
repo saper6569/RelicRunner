7cf2e52d30d8341cec54a249d271acb397a3a82f
ryderwest <ryder.west@icloud.com>
2024-01-16 22:59:05 -0500

    added comments

--- core/src/com/relicraider/screens/utilities/HUD.java
+++ core/src/com/relicraider/screens/utilities/HUD.java
@@ -1,3 +1,8 @@
+/* Relic Raider ; Final Project ICS4U
+   Sanija, Ryder, Amin
+   December 15th, 2023 - January 16th, 2024
+   Hud Class
+ */
 package com.relicraider.screens.utilities;
 
 import com.badlogic.gdx.Game;
@@ -24,7 +29,9 @@ import com.relicraider.screens.MainMenu;
 import com.relicraider.screens.gamescreens.*;
 import com.relicraider.screens.utilities.Button;
 
+//Hud class implements the Disposable super class
 public class HUD implements Disposable {
+//ATTRIBUTES
     private Viewport viewport;
     public Stage stage;
     private Player player;
@@ -47,34 +54,42 @@ public class HUD implements Disposable {
     private String room;
     private RelicRaider game;
 
+    /**
+     * Primary Constructor for HUD
+     * @param game - Game Object
+     * @param spriteBatch - libGDX Sprite Batch object
+     * @param player - Player Object
+     */
     public HUD (RelicRaider game, SpriteBatch spriteBatch, Player player) {
         this.player = player;
         this.game = game;
+        //Import Images, create textures using each image
         fullHeart = new Texture(Gdx.files.internal("Other/fullHeart.png"));
         halfHeart = new Texture(Gdx.files.internal("Other/halfHeart.png"));
         emptyHeart = new Texture(Gdx.files.internal("Other/emptyHeart.png"));
 
-        OrthographicCamera camera = new OrthographicCamera();
-        viewport = new FitViewport(SetupVariables.WIDTH, SetupVariables.HEIGHT, camera);
-        stage = new Stage(viewport, spriteBatch);
-        Gdx.input.setInputProcessor(stage);
+        OrthographicCamera camera = new OrthographicCamera(); //Create new camera
+        viewport = new FitViewport(SetupVariables.WIDTH, SetupVariables.HEIGHT, camera); //Camera's viewport is set width and height, found in setup variables class
+        stage = new Stage(viewport, spriteBatch); //Create new stage
+        Gdx.input.setInputProcessor(stage); //Set inputs onto stage
 
-        enterButton = new Button("ENTER", 0, 0, stage, 24);
-        showButton = false;
+        enterButton = new Button("ENTER", 0, 0, stage, 24); //Create button for entering a new room
+        showButton = false; //Dont show button unless player is close to a door
         room = null;
 
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("KenPixel Mini Square.ttf"));
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("KenPixel Mini Square.ttf")); //Import font
         FreeTypeFontGenerator.FreeTypeFontParameter fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        fontParameter.magFilter = Texture.TextureFilter.MipMapNearestNearest;
-        fontParameter.size = 18;
+        fontParameter.magFilter = Texture.TextureFilter.MipMapNearestNearest; //Set font texture
+        fontParameter.size = 18; //Set font size
 
         BitmapFont font = generator.generateFont(fontParameter);
-        Label.LabelStyle labelStyle = new Label.LabelStyle(font, Color.WHITE);
+        Label.LabelStyle labelStyle = new Label.LabelStyle(font, Color.WHITE); //Create new label with font
 
-        Table table = new Table();
-        table.top().left();
+        Table table = new Table(); //Create new table for stats to be in
+        table.top().left(); //Set table to top left of screen
         table.setFillParent(true);
 
+//Fill table with stats
         healthL = new Label (" HEALTH ", labelStyle);
         healthLabel = new Label(String.format("%03d", player.getHealth()), labelStyle);
         roomL = new Label (" ROOM ", labelStyle);
@@ -94,20 +109,24 @@ public class HUD implements Disposable {
         table.row();
         table.add(relicsLable).padTop(10).padLeft(5);
 
+//Add enter button to hud, put to the bottom right of the screen
         Table enterTable = new Table();
         enterTable.bottom().right();
         enterTable.setFillParent(true);
         enterTable.add(enterButton.getButton()).padBottom(20).padRight(5);
 
+//Add hearts to hud
         hearts = new Table();
         hearts.bottom().left();
         hearts.setFillParent(true);
 
-        stage.addActor(table);
+
+        stage.addActor(table); //Add table to stage
         stage.addActor(enterTable);
-        drawHearts(Player.playerHealth, 1);
+        drawHearts(Player.playerHealth, 1); //Draw players health
         stage.addActor(hearts);
 
+        //If player clicks enter button call playerIsEntering method
         enterButton.getButton().addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -116,17 +135,22 @@ public class HUD implements Disposable {
         });
     }
 
+    /**
+     * Method to draw player's hearts
+     * @param health - amount of health player has
+     * @param counter - Counter variable for players hearts
+     */
     private void drawHearts(int health, int counter) {
-        if (health < 5 && counter > 10) {
+        if (health < 5 && counter > 10) { //If player's health is less than 5 and counter is greater than 10, return
             return;
-        } else if (health < 5) {
-            hearts.add(new Image(emptyHeart)).padBottom(20).height(40).width(40).padLeft(5);
+        } else if (health < 5) { //If player's health is less than 5
+            hearts.add(new Image(emptyHeart)).padBottom(20).height(40).width(40).padLeft(5); //Draw an empty heart
             drawHearts(health, counter + 1);
-        } else if (health < 10) {
-            hearts.add(new Image(halfHeart)).padBottom(20).height(40).width(40).padLeft(5);
+        } else if (health < 10) { //If players health is less than 10
+            hearts.add(new Image(halfHeart)).padBottom(20).height(40).width(40).padLeft(5); //Draw half a heart
             drawHearts(health - 5, counter + 1);
-        } else {
-            hearts.add(new Image(fullHeart)).padBottom(20).height(40).width(40).padLeft(5);
+        } else { //If players health is not true for any of the above
+            hearts.add(new Image(fullHeart)).padBottom(20).height(40).width(40).padLeft(5); //Draw a full heart
             drawHearts(health - 10, counter + 1);
         }
     }
@@ -148,6 +172,9 @@ public class HUD implements Disposable {
         }
     }
 
+    /**
+     * Method to show set enter button to show what room the door goes to, set screen to the new room
+     */
     private void playerIsEntering() {
         if (room.equals("room 1")) {
             ((Game) Gdx.app.getApplicationListener()).setScreen(new Room1(game, doorX, doorY));
@@ -164,38 +191,123 @@ public class HUD implements Disposable {
         }
     }
 
+    /**
+     * Method to dispose assets used in the hud
+     */
     @Override
     public void dispose() {
         stage.dispose();
     }
 
+    /**
+     * Method to get viewport object
+     * @return - Viewport object
+     */
     public Viewport getViewport() {
         return viewport;
     }
 
+    /**
+     * Method to set viewport object
+     * @param viewport - New Viewport
+     */
     public void setViewport(Viewport viewport) {
         this.viewport = viewport;
     }
 
+    /**
+     * Method to get stage object
+     * @return - Stage Object
+     */
     public Stage getStage() {
         return stage;
     }
 
+    /**
+     * Method to set stage object
+     * @param stage - New stage
+     */
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
+    /**
+     * Method to get player object
+     * @return - Player Object
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * Method to set player object
+     * @param player - New Player Object
+     */
     public void setPlayer(Player player) {
         this.player = player;
     }
 
+    /**
+     * Method to set if button is to be shown
+     * @param showButton - True - If button is to be shown, False -  if it isnt
+     */
+    public void setShowButton(boolean showButton) {
+        this.showButton = showButton;
+    }
+
+    /**
+     * Method to get current room player is in
+     * @return - current room player is in
+     */
+    public String getRoom() {
+        return room;
+    }
+
+    /**
+     * Method to set current room player is in
+     * @param room - New Room player is in
+     */
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    /**
+     * Method to get x position of a door
+     * @return - X position of door
+     */
+    public float getDoorX() {
+        return doorX;
+    }
+
+    /**
+     * Method to set x position of a door
+     * @param doorX - New x position of door
+     */
+    public void setDoorX(float doorX) {
+        this.doorX = doorX;
+    }
+
+    /**
+     * Method to get y position of a door
+     * @return - Y position of door
+     */
+    public float getDoorY() {
+        return doorY;
+    }
+
+    /**
+     * Method to set y position of a door
+     * @param doorY - New y position of door
+     */
+    public void setDoorY(float doorY) {
+        this.doorY = doorY;
+    }
+
+//METHODS NOT USED YET
     public Label getHealthL() {
         return healthL;
     }
+
 
     public void setHealthL(Label healthL) {
         this.healthL = healthL;
@@ -285,31 +397,5 @@ public class HUD implements Disposable {
         return showButton;
     }
 
-    public void setShowButton(boolean showButton) {
-        this.showButton = showButton;
-    }
 
-    public String getRoom() {
-        return room;
-    }
-
-    public void setRoom(String room) {
-        this.room = room;
-    }
-
-    public float getDoorX() {
-        return doorX;
-    }
-
-    public void setDoorX(float doorX) {
-        this.doorX = doorX;
-    }
-
-    public float getDoorY() {
-        return doorY;
-    }
-
-    public void setDoorY(float doorY) {
-        this.doorY = doorY;
-    }
 }
